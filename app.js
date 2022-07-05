e74549b6cd25daf9dae3f6a1b0d49f8eab13e527
gabriel nuñez <nunez6428@gmail.com>
2022-07-04 21:31:32 -0300

    agregado

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-// solicitar al cliente agregar producto/s para calcular monto total de la compra + costo de envio 
-while (true) {
-   let precioCuchillo = parseFloat(prompt("ingrese precio del cuchillo"))
-   
+// solicitar el nombre al cliente
+let ingresarNombre = prompt("ingresa su nombre o ESC para interrumpir");
+let texto = " ";
+while (ingresarNombre != "ESC" && ingresarNombre != "esc") {
+  texto += ingresarNombre + " ";
+  ingresarNombre = prompt("ingrese su nombre o ESC para interrumpir");
 }
+alert("Hola Bienvenido a nuestra tiendagit" + texto);
+// solicitar al cliente agregar producto/s para calcular monto total de la compra
+let precioCuchillo = parseFloat(prompt("ingrese precio del primer cuchillo"));
+
+let precioCuchillo2 = parseFloat(prompt("ingrese precio del segundo cuchillo"));
+
+function sumarProducto(precioCuchillo, precioCuchillo2) {
+  let precioTotal = precioCuchillo + precioCuchillo2;
+  return precioTotal;
+}
+console.log(sumarProducto(precioCuchillo, precioCuchillo2));
+
+//costo total
+precioTotal = (sumarProducto(precioCuchillo, precioCuchillo2));
+let envio = 500;
+function costoTotal(precioTotal, envio) {
+  let totalCompra = precioTotal + envio;
+  return totalCompra;
+}
+console.log(costoTotal(precioTotal, envio));
+totalCompra = (costoTotal(precioTotal, envio));
+alert("el total de la compra con el envio es de: " +totalCompra);
