2e62a80949fbfd90534d8244216052c2dd6852d9
gabriel nuñez <nunez6428@gmail.com>
2022-07-11 19:39:32 -0300

    agregado array

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ while (ingresarNombre != "ESC" && ingresarNombre != "esc") {
   texto += ingresarNombre + " ";
   ingresarNombre = prompt("ingrese su nombre o ESC para interrumpir");
 }
-alert("Hola Bienvenido a nuestra tiendagit" + texto);
+alert("Hola" + texto + "Bienvenido a nuestra tienda");
 // solicitar al cliente agregar producto/s para calcular monto total de la compra
 let precioCuchillo = parseFloat(prompt("ingrese precio del primer cuchillo"));
 
@@ -27,3 +27,9 @@ function costoTotal(precioTotal, envio) {
 console.log(costoTotal(precioTotal, envio));
 totalCompra = (costoTotal(precioTotal, envio));
 alert("el total de la compra con el envio es de: " +totalCompra);
+
+const tiendaCuchillo = {id:1, producto: "cuchillo asado", precio: "5.476"};
+const array =[tiendaCuchillo, {id: 2, producto: "cuchillo alpaca", precio: "2.476"}];
+array.push({id:3, producto: "cuchillo ciervo", precio: "6.000"});
+
+console.log(array)
